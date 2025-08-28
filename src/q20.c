a72745d2257816dfff58031fe22df9397f8a2661
richa sharma <shricha4@gmail.com>
2025-08-28 20:22:48 +0545

    third

--- src/q20.c
+++ src/q20.c
@@ -4,10 +4,9 @@ int main() {
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
-    int multiple_of_3 = (num / 3) * 3 == num;
-    int multiple_of_5 = (num / 5) * 5 == num;
-
-    printf("%d\n", multiple_of_3 || multiple_of_5);
+    int is_multiple_of_3 = (num / 3) * 3 == num;
+    int is_multiple_of_5 = (num / 5) * 5 == num;
+    printf("%d\n", is_multiple_of_3 || is_multiple_of_5);
 
     return 0;
 }
