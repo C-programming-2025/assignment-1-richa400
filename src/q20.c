844028ac65564902267a18f3f1bc31b4fa224cc5
richa sharma <shricha4@gmail.com>
2025-08-28 20:05:24 +0545

    third

--- src/q20.c
+++ src/q20.c
@@ -1,14 +1,10 @@
-
-
 #include <stdio.h>
 
 int main() {
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
-    int is_multiple = (num % 3 == 0) || (num % 5 == 0);
-
-    printf("Is %d a multiple of 3 or 5? %d\n", num, is_multiple);
+    printf("%d\n", (num % 3 == 0) || (num % 5 == 0));
 
     return 0;
 }
