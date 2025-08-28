9471d5aac7637d44fb2e09d9c5fdef7a2c418a60
richa sharma <shricha4@gmail.com>
2025-08-28 19:48:45 +0545

    second

--- src/q12.c
+++ src/q12.c
@@ -5,11 +5,11 @@
 int main() {
     int a, b, c;
     int result;
-    printf("Enter value for a: ");
+    printf("Enter value a: ");
     scanf("%d", &a);
-    printf("Enter value for b: ");
+    printf("Enter value b: ");
     scanf("%d", &b);
-    printf("Enter value for c: ");
+    printf("Enter value c: ");
     scanf("%d", &c);
     result = (a > b) && (c != 0);
     printf("%d\n", result);
